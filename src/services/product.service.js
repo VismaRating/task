f7afce5ea72d1522916c32bb0f195f614b38b7e6
Christo.Matrakou <chrismatrakou@gmail.com>
2021-11-09 19:03:29 +0100

    filter

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,21 +1,20 @@
 const data = require('../payload.json');
-const filtered_keys = ['took', 'timed_out','cvr','status','telefonNummer'];
-// const language = 'en'
-// var translator = require('translate-json-object')(); 
-
-// translator.init({
-//     googleApiKey: 'AIzaSyCJLL9sKN_P6gAU6bqG2RhaC1TBocVxPJ8',
-// });
-
-// translator.translate(example, 'es')
-//     .then(function(data) {
-//       console.log(data);
-//     }).catch(function(err) {
-//       console.log('error ', err)
-//     });
+var fs = require('fs');
+const filtered_keys = ['cvrNummer', 'status', 'telefonNummer'];
 
 const getAll = function(){
-    return filter(data);
+    var outputFilename = 'my.json';
+
+    fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
+    if(err) {
+        console.log(err);
+        }else {
+        console.log("JSON saved to " + outputFilename);
+        }
+    }); 
+    var obj = data['hits']['hits'][0]['_source']['Vrvirksomhed'];
+    //return obj;
+    return filter(obj);
 }
 
 const filter = function(raw_data){
