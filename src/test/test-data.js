7b419c410bcf3c612b85270c5698f54b9cce4bbf
Christo.Matrakou <chrismatrakou@gmail.com>
2021-11-09 23:48:53 +0100

    update tests

--- src/test/test-data.js
+++ src/test/test-data.js
@@ -1,21 +1,66 @@
 var expect = require('chai').expect,
   data = require('../database/payload.json');
+
+var d = data['hits']['hits'][0]['_source']['Vrvirksomhed'];
+var arrayLength = d.beliggenhedsadresse.length;
+var lastIndex = arrayLength - 1;
   
-describe("Test data have valid keys", function (){
-  it("expects valid data", function(){
-      var d = data['hits']['hits'][0]['_source']['Vrvirksomhed'];
-      var arrayLength = d.beliggenhedsadresse.length;
-      lastIndex = arrayLength - 1;
-       
+describe("Test data have valid key navn", function (){
+  it("expects valid data navn ", function(){
       expect(d.navne[0]).to.have.property('navn');
-      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('vejnavn');
-      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('husnummerFra');
-      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('postnummer');
-      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('postdistrikt');
-      expect(d).to.have.property('telefonNummer');
-      expect(d).to.have.property('obligatoriskEmail');  
-      expect(d.virksomhedsstatus[0]).to.have.property('status');
-      expect(d.virksomhedsform[0]).to.have.property('langBeskrivelse');  
-      expect(d.hovedbranche[0]).to.have.property('branchekode');  
   })
 })
+
+describe("Test data have valid key vejnavn", function (){
+  it("expects valid data vejnavn", function(){
+      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('vejnavn');
+  })
+})
+
+describe("Test data have valid key husnummerFra", function (){
+  it("expects valid data husnummerFra", function(){
+      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('husnummerFra');
+  })
+})
+
+describe("Test data have valid key postnummer", function (){
+  it("expects valid data postnummer", function(){
+      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('postnummer');
+    })
+  })
+  
+describe("Test data have valid key postdistrikt", function (){
+  it("expects valid data postdistrikt", function(){
+      expect(d.beliggenhedsadresse[lastIndex]).to.have.property('postdistrikt');
+    })
+  })
+    
+describe("Test data have valid key telefonNummer", function (){
+  it("expects valid data telefonNummer", function(){
+      expect(d).to.have.property('telefonNummer');
+    })
+  })
+
+describe("Test data have valid key obligatoriskEmail", function (){
+  it("expects valid data obligatoriskEmail", function(){
+      expect(d).to.have.property('obligatoriskEmail');  
+    })
+  })
+        
+describe("Test data have valid key status", function (){
+  it("expects valid data status", function(){
+      expect(d.virksomhedsstatus[0]).to.have.property('status');
+    })
+  })
+          
+describe("Test data have valid key langBeskrivelse", function (){
+  it("expects valid data langBeskrivelse", function(){
+      expect(d.virksomhedsform[0]).to.have.property('langBeskrivelse');  
+    })
+  })
+            
+describe("Test data have valid key branchekode", function (){
+  it("expects valid data branchekode", function(){
+      expect(d.hovedbranche[0]).to.have.property('branchekode');   
+    })
+  })
